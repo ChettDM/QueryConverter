33be6a7ba172cbeb35c2895a5754152a98b7ecac
Chett Michael <Chett.Michael@gmail.com>
2022-08-10 23:11:34 -0500

    Added Conversion Functionality

---
+++ Utilities/ExecuteSqlProcedureConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace QueryConverter.Utilities;
+
+public class ExecuteSqlProcedureConverter
+{
+    private readonly string _textToConvert;
+    
+    public string ConvertedText => ConvertQuery(_textToConvert);
+    
+    public ExecuteSqlProcedureConverter(string textToConvert)
+    {
+        _textToConvert = textToConvert;
+    }
+
+    private string ConvertQuery(string textToConvert)
+    {
+        if (string.IsNullOrEmpty(textToConvert)) throw new Exception("No Text Provided");
+        
+        var textToConvertSections = textToConvert.Split("',N'");
+
+        if (textToConvertSections.Length != 2) throw new Exception("More than one Split");
+        
+        var textToConvertFirstPart = textToConvertSections[0];
+        var textToConvertSecondPart = textToConvertSections[1];
+
+        textToConvertFirstPart = textToConvertFirstPart[21..];
+
+        var textToConvertSecondPartSections = textToConvertSecondPart.Split(',');
+
+        var convertedStringBuilder = new StringBuilder();
+        convertedStringBuilder.AppendLine("-- Declarations");
+        convertedStringBuilder.Append("DECLARE ");
+
+        var index = 0;
+        for (; index < textToConvertSecondPartSections.Length; index++)
+        {
+            var item = textToConvertSecondPartSections[index];
+            if (!item.Contains('='))
+                convertedStringBuilder.Append(item + ", ");
+            else break;
+        }
+        convertedStringBuilder.Remove(convertedStringBuilder.Length - 3, 3);
+
+        convertedStringBuilder.AppendLine(";");
+        convertedStringBuilder.AppendLine();
+        convertedStringBuilder.AppendLine("-- Sets");
+        for (; index < textToConvertSecondPartSections.Length; index++)
+        {
+            convertedStringBuilder.AppendLine(
+                "SET " + 
+                textToConvertSecondPartSections[index] + 
+                ";");
+        }
+
+        convertedStringBuilder.AppendLine();
+        convertedStringBuilder.AppendLine("-- Query");
+        convertedStringBuilder.AppendLine(textToConvertFirstPart);
+
+        return convertedStringBuilder.ToString();
+    }
+}
---
+++ ViewModels/ExecuteSqlProcedureConverterViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVM;
+using MVVM.Arguments;
+using QueryConverter.Utilities;
+
+namespace QueryConverter.ViewModels;
+
+public class ExecuteSqlProcedureConverterViewModel : DataContext
+{
+    public string _unConvertedText;
+    public string _message;
+
+    public string UnConvertedText
+    {
+        get => _unConvertedText;
+        set
+        {
+            if (_unConvertedText == value) return;
+            _unConvertedText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Message
+    {
+        get => _message;
+        set
+        {
+            if (_message == value) return;
+            _message = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsOpen));
+        }
+    }
+
+    public bool IsOpen
+    {
+        get => string.IsNullOrEmpty(_message);
+    }
+    
+    public Command Convert { get; }
+
+    public ExecuteSqlProcedureConverterViewModel()
+    {
+        Convert = new Command();
+        Convert.ExecuteActions += ConvertOnExecuteActions;
+    }
+
+    private void ConvertOnExecuteActions(object parameters, ExecuteCommandArguments arg)
+    {
+        try
+        {
+            Clipboard.SetText(new ExecuteSqlProcedureConverter(UnConvertedText).ConvertedText);
+            CreateMessage("Copied to Clipboard");
+        }
+        catch (Exception e)
+        {
+            Clipboard.SetText(e.Message);
+            CreateMessage(e.Message);
+        }
+    }
+
+    private void CreateMessage(string message)
+    {
+        // Message = message;
+        // var task = new Task(() =>
+        // {
+        //     Task.Delay(3000);
+        //     if (message == Message)
+        //         Message = "";
+        // });
+        // task.Start();
+    }
+}
